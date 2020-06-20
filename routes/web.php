b8304bac87f4bca72fce371a49565d02470b587a
”YYamaguchi” <gg09.tjsthysyj.yy28@gmail.com>
2020-06-20 13:08:20 +0000

    PHP 12 tasks

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,17 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add') -> middleware('auth');
     // task 4
-    Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::get('profile/create', 'Admin\ProfileController@add') -> middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileController@edit') -> middleware('auth');
 
 });
 
 // task 3
 Route::get('XXX', 'Admin\AAAController@bbb');
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
